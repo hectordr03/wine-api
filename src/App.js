b2f3874cf8d15da702d57c5851b5ca411e0a1d64
Student_GA126 <student_ga126@psxga126.lan>
2020-08-03 15:15:05 -0400

    Displayed wine list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './App.css';
 
-import Wines from './components/Wines'
+import Wines from './components/Wines';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Sup son</h1>
-      <Wines />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      info: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://myapi-profstream.herokuapp.com/api/aeee9e/wines')
+      .then(res => {
+        this.setState({
+          info: res.data
+        })
+        console.log(res.data)
+      })
+      .catch(err => console.error(err.message))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Wines info={this.state.info} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Wines.js
+++ src/components/Wines.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Wines extends Component {
-    constructor(props) {
-        super(props)
-    }
-    
-    render() {
-        return (
-            <div className='wines-component'>
-                <h3>Wine Component</h3>
-            </div>
-        )
-    }
+const Wines = (props) => {
+    return (
+        <div className='wines-list' >
+            <h2>Wine List</h2>
+            {props.info.map(wine => (
+                <div key={wine.id} className='wine' >
+                    <img src={wine.picture} />
+                    <h4>{wine.name}</h4>
+                    <p>A {wine.year} wine from {wine.region}, {wine.country}</p>
+                    <p>${wine.price}</p>
+                    <p>{wine.description}</p>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Wines;
