6aa3eaabd44e5a50e4dac7557636b8c9d6485f12
Student_GA126 <student_ga126@psxga126.lan>
2020-08-03 14:33:29 -0400

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Wines from './components/Wines'
+
 function App() {
   return (
     <div className="App">
-      
+      <h1>Sup son</h1>
+      <Wines />
     </div>
   );
 }
