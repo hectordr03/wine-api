a0071fc046bebd8c84986d75c072b48f0defa512
Student_GA126 <student_ga126@psxga126.lan>
2020-08-03 14:33:04 -0400

    installed axios

--- src/components/Wines.js
+++ src/components/Wines.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class Wines extends Component {
+    constructor(props) {
+        super(props)
+    }
+    
     render() {
         return (
-            <div>
-                
+            <div className='wines-component'>
+                <h3>Wine Component</h3>
             </div>
         )
     }
