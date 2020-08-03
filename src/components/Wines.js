22981faf5108cc1f09f4ca089d2bdf93d0dde514
Student_GA126 <student_ga126@psxga126.lan>
2020-08-03 14:27:27 -0400

    Created wines component

---
+++ src/components/Wines.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Wines extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Wines;
